aec4f622d5cda7edb5b2999890848e2d951142a3
kchaboud <kevin.chaboud@hotmail.fr>
2018-02-17 17:05:34 +0100

    Moving platform behaviour: loop & backforth

--- Assets/MovingPlatformAdvanced.cs
+++ Assets/MovingPlatformAdvanced.cs
@@ -7,11 +7,13 @@ public class MovingPlatformAdvanced : MonoBehaviour {
     public GameObject platform;
     public Transform[] points; //size must be > 2
     public float speed;
-    public enum Behaviour {loop, continuous}; //default loop
+        public enum Behaviour {loop, backforth}; //loop: 1->2->3->1->2... backforth: 1->2->3->2->1->2...
+    public Behaviour behaviour = Behaviour.loop;
 
     private Rigidbody2D rb;
     private int currentGoal = 1;
     private Vector3 currentDirection;
+    private bool backforthGo = true; //go = true, return = false
 
     private void Start()
     {
@@ -41,7 +43,30 @@ public class MovingPlatformAdvanced : MonoBehaviour {
 
     private int NextPoint()
     {
+        if (behaviour == Behaviour.loop) return NextPointWithLoop();
+        else return NextPointWithBackforth();
+    }
+
+    private int NextPointWithLoop()
+    {
         return (currentGoal + 1) % points.Length;
+    }
+
+    private int NextPointWithBackforth()
+    {
+        //BackforthGo change
+        if (backforthGo && (currentGoal + 1) == points.Length)
+        {
+                backforthGo = false;
+        }
+        else if (!backforthGo && (currentGoal - 1) == -1)
+        {
+            backforthGo = true;
+        }
+
+        //Return next point
+        if (backforthGo) return currentGoal + 1;
+        else return currentGoal - 1;
     }
 
     private void ChangeNextPoint()
